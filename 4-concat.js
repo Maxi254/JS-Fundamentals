e15994e46f5289c96d3e690e05e75995e38ed9b6
Maxi254 <ocholamaxwell543@gmail.com>
2025-11-09 01:13:56 -0800

    test

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the arguments (will be undefined if not provided)
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print in the required format
+console.log(firstArg + " is " + secondArg);
